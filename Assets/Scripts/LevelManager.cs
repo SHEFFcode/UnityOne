6dedbc116bf8663104b431c4e599a1f47e67f1de
SHEFFcode <sheff@sheffmachine.com>
2016-10-24 21:39:41 -0700

    create a game scene and hook up the go back to start scene button

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,9 +5,15 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name) {
 		Debug.Log ("Level load requested for " + name);
+		Application.LoadLevel (name);
 	}
 
 	public void QuitRequest() {
 		Debug.Log ("Quit Requested");
+	}
+
+	public void GoBackToScene(string name) {
+		Debug.Log("Going back to scene" + name);
+		Application.LoadLevel (name);
 	}
 }
